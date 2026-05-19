dd86ba9720568fb7991eaf024c58581f05dedcf4
smriti-s21 <23f2000599@ds.study.iitm.ac.in>
2026-05-19 13:24:04 +0530

    Use DATABASE_URL as-is for Railway

--- api/core/database.py
+++ api/core/database.py
@@ -9,15 +9,9 @@ load_dotenv(override=False)
 DATABASE_URL = os.environ.get('DATABASE_URL') or 'postgresql://agripulse:agripulse123@localhost:5432/agripulse'
 
 import logging
-logging.info(f'DATABASE_URL host: {DATABASE_URL.split("@")[-1] if "@" in DATABASE_URL else "local"}')
+logging.warning(f'DB connecting to: {DATABASE_URL.split("@")[-1] if "@" in DATABASE_URL else "local"}')
 
-base_url = DATABASE_URL.split('?')[0]
-is_railway = 'rlwy.net' in base_url or 'railway.internal' in base_url
-
-if is_railway:
-    engine = create_engine(base_url, connect_args={'sslmode': 'require', 'sslrootcert': 'disable'})
-else:
-    engine = create_engine(base_url)
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
