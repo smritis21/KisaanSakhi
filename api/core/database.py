daa9d1ad0c859b786694b31a2d5305e1d94cb76a
smriti-s21 <23f2000599@ds.study.iitm.ac.in>
2026-05-19 13:19:43 +0530

    Debug: log DB host, try sslrootcert=disable

--- api/core/database.py
+++ api/core/database.py
@@ -8,12 +8,14 @@ load_dotenv(override=False)
 
 DATABASE_URL = os.environ.get('DATABASE_URL') or 'postgresql://agripulse:agripulse123@localhost:5432/agripulse'
 
+import logging
+logging.info(f'DATABASE_URL host: {DATABASE_URL.split("@")[-1] if "@" in DATABASE_URL else "local"}')
+
 base_url = DATABASE_URL.split('?')[0]
 is_railway = 'rlwy.net' in base_url or 'railway.internal' in base_url
 
 if is_railway:
-    DATABASE_URL = base_url
-    engine = create_engine(DATABASE_URL, connect_args={'sslmode': 'allow'})
+    engine = create_engine(base_url, connect_args={'sslmode': 'require', 'sslrootcert': 'disable'})
 else:
     engine = create_engine(base_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
